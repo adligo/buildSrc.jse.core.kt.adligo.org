9599576102942c8677bc755eeac7961ebb8ad726
Scott Morgan <scott@adligo.com>
2022-03-10 21:37:40 -0600

    Adding in more dependency extractions.

---
+++ src/main/kotlin/org/adligo/kt/jse/core/build/MockitoDeps.kt
@@ -0,0 +1,37 @@
+package org.adligo.kt.jse.core.build
+
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+
+/**
+ * This provides the dependencies for Mockito see methods
+ *
+ * @author scott
+ * <pre><code>
+ * ---------------- Apache ICENSE-2.0 --------------------------
+ *
+ * Copyright 2022 Adligo Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </code><pre>
+ */
+ 
+object MockitoDeps {
+
+  /**
+  * provides a way for other projects to depend on gwt
+  */
+  fun dependsOnMockito(gradleCallback : I_GradleCallback) {
+    gradleCallback.implementation("org.mockito:mockito-all:1.10.19")
+  }
+}
---
+++ src/main/kotlin/org/adligo/kt/jse/core/build/MockitoExtDeps.kt
@@ -0,0 +1,50 @@
+package org.adligo.kt.jse.core.build
+
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+
+/**
+ * This provides the dependencies for the
+ * {@link <a href="https://github.com/adligo/mockto_ext.adligo.org">mockto_ext/a>
+ * project and also exports the ability to depend on mockto_ext with the function;<br/>
+ * dependsOnMockitoExt
+ *
+ * @author scott
+ * <pre><code>
+ * ---------------- Apache ICENSE-2.0 --------------------------
+ *
+ * Copyright 2022 Adligo Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </code><pre>
+ */
+
+object MockitoExtDeps {
+
+    /**
+     * provides a way for other projects to depend on i_ctx
+     */
+    fun dependsOnMockitoExt(gradleCallback : I_GradleCallback) {
+        has(gradleCallback)
+        gradleCallback.implementation(gradleCallback.projectFun("mockito_ext.adligo.org"))
+    }
+
+    fun has(gradleCallback : I_GradleCallback) {
+        MockitoDeps.dependsOnMockito(gradleCallback)
+    }
+
+    fun testsHave(gradleCallback : I_GradleCallback) {
+        dependsOnMockitoExt(gradleCallback)
+        Tests4j4jjDeps.dependsOnTests4j4jj(gradleCallback)
+    }
+}
